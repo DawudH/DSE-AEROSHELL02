e34449832f0b88ea3ea057182a18e8ccdad1fd5d
DawudH <d.d.hage@student.tudelft.nl>
2015-04-29 09:38:49 +0200

    matlab update

--- SOFTWARE/orbitcheck.m
+++ SOFTWARE/orbitcheck.m
@@ -2,9 +2,11 @@ clc
 clear all 
 close all
 
+do_plot = false;
+write_to_file = true;
 
 m = 10000; %[kg]
-CD = 1;
+
 d = 12; % [m] diameter heatshield
 S = 12^2*pi/4;
 R_m = 6794000/2; %[m]
@@ -14,30 +16,48 @@ dt = 1;
 h_atmos = 104 *10^3; % [m]
 M_mars = 6.419*10^23; %[kg]
 G = 6.673*10^-11; %[N*(m/kg)^2]
-rx = -1.22*R_m:-0.001*R_m:-1.23*R_m;
+
+% changing variables
+rx = -1.2*R_m:-0.01*R_m:-1.25*R_m;
+CD = 0.5:0.1:1.4;
 
 cc = parula(length(rx)+3);
-figure('name','orbits')
-for i=1:length(rx)
-    [out, R, V, A] = orbitmodel_new(rx(i),ry,R_m,m,CD,S,v,dt,h_atmos,M_mars,G);
-    out
-    t = 0:dt:(length(R)*dt-dt);
-    subplot(3,1,1)
-    hold on
-    grid on
-    plot(t,sqrt(R(:,1).^2 + R(:,2).^2 + R(:,3).^2),'color',cc(i,:));
-    subplot(3,1,2)
-    hold on
-    grid on
-    plot(t,sqrt(V(:,1).^2 + V(:,2).^2 + V(:,3).^2),'color',cc(i,:));
-    subplot(3,1,3)
-    hold on
-    grid on
-    plot(t,sqrt(A(:,1).^2 + A(:,2).^2 + A(:,3).^2),'color',cc(i,:));
+if do_plot
+    figure('name','orbits')
+end
+
+if write_to_file
+    fid = fopen('orbit_true_or_false.txt');
+end
+for k = 1:length(CD)
+    for i=1:length(rx)
+        [out, R, V, A] = orbitmodel_new(rx(i),ry,R_m,m,CD(k),S,v,dt,h_atmos,M_mars,G);
+        disp(['rx = ' num2str(rx(i)) ' [m], in atmosphere: ' num2str(out.inatmos) ', crashed: ' num2str(out.crash) ', in orbit: ' num2str(out.inorbit) ', acceleration: ' num2str(out.maxaccel) ])
+        if write_to_file
+            fprintf(fid,'%11.1f, %4.2f, %d, %d, %d, %5.3f',rx(i),CD(k),out.inatmos,out.crash,out.inorbit,out.maxaccel);
+        end
+        t = 0:dt:(length(R)*dt-dt);
+        if do_plot 
+            subplot(3,1,1)
+            hold on
+            grid on
+            plot(t,sqrt(R(:,1).^2 + R(:,2).^2 + R(:,3).^2),'color',cc(i,:));
+            subplot(3,1,2)
+            hold on
+            grid on
+            plot(t,sqrt(V(:,1).^2 + V(:,2).^2 + V(:,3).^2),'color',cc(i,:));
+            subplot(3,1,3)
+            hold on
+            grid on
+            plot(t,sqrt(A(:,1).^2 + A(:,2).^2 + A(:,3).^2),'color',cc(i,:));
+        end
+    end
 end
 
 
-
+if write_to_file
+    fclose(fid);
+end
 
 
 
--- SOFTWARE/orbitmodel_new.m
+++ SOFTWARE/orbitmodel_new.m
@@ -1,5 +1,13 @@
 function [out, R, V, a] = orbitmodel_new(rx,ry,R_m,m,CD,S,v,dt,h_atmos,M_mars,G)
 
+
+% define output 
+out.inorbit = false;
+out.inatmos = false;
+out.rightaccel = false;
+out.maxaccel = 0;
+out.crash = false;
+
 R(1,:) = [rx, ry, 0];
 V(1,:) = [0, -v, 0];
 [ g, p, T, rho, asound ] = mars_atmosphere(norm(R(1,:)) - R_m);
@@ -7,7 +15,6 @@ ag(1,:) = -g * R(1,:)/norm(R(1,:));
 ad(1,:) = - (V(1,:) / norm(V(1,:))) * CD * 0.5* rho * norm(V(1,:))^2 * S / m;
 a(1,:) = ag(1,:) + ad(1,:);
 i = 1;
-in_atmos = false;
 V_esc = sqrt(G*M_mars * 2 / (h_atmos + R_m)); % escape velocity at the border of the atmosphere
 while true
     
@@ -21,46 +28,42 @@ while true
     
     % if the s/c has ever been in the atmosphere in_atmus => true
     if (norm(R(i,:)) < (R_m + h_atmos)) 
-        in_atmos = true;
+        out.inatmos = true;
+        
     end
         
     % if s/c has been in atmos and is leaving atmos again, stop simulation
     % and check velocity and max deceleration...
-    if in_atmos && ((norm(R(i,:)) > (R_m + h_atmos))) 
+    if out.inatmos && ((norm(R(i,:)) > (R_m + h_atmos))) 
 
         admag = sqrt(ad(:,1).^2 + ad(:,2).^2 + ad(:,3).^2);
-        if (norm(V(end,:)) < V_esc) && (max(admag)/9.81 < 3)
-            out = true;
-        else
-            out = false;
+        out.maxaccel = max(admag)/9.81;
+        %disp(['Vesc = ' num2str(V_esc) ' [m/s], Vend = ' num2str(norm(V(end,:))) ' [m/s]'])
+        if (norm(V(end,:)) < V_esc)
+            if (out.maxaccel < 3)
+               
+                out.rightaccel = true;
+                
+            end
+            out.inorbit = true; 
         end
         break;
         
     end
     
     % check if crashed..
-    if in_atmos && (norm(R(i,:)) < R_m) 
-        out = false;
+    if out.inatmos && (norm(R(i,:)) < R_m) 
+        out.crash = true;
         break;
     end
 
     % if passed the planet without getting into the atmosphere..
-    if (in_atmos == false) && (R(i,2) < -(R_m + h_atmos))
+    if (out.inatmos == false) && (R(i,2) < -(R_m + h_atmos))
         V_esc_0 = sqrt(G*M_mars * 2 / norm(R(1,:))); % escape velocity at the inital point
         % check if initial velocity is to fast to get into orbit.. ?
-        if v > V_esc_0
-            to_fast = true;
-        else
-            to_fast = false;
+        if v < V_esc_0
+            out.inorbit = true;
         end
-        
-        
-        if to_fast
-            out = false;
-        else
-            out = true;
-        end
-       
         break;
     end
     
