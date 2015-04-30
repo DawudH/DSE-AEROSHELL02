33b6ae426082044209a3da53dddfb1d4a4a8906c
Twan Keijzer <twankeijzer@hetnet.nl>
2015-04-30 11:46:02 +0200

    a lot!

--- SOFTWARE/orbital_model/constants.m
+++ SOFTWARE/orbital_model/constants.m
@@ -1,4 +1,6 @@
- omega_m = 2*pi / (24.6229622 * 3600 ); %[rad/sec]
+addpath('..\mars_atmosphere')
+
+omega_m = 2*pi / (24.6229622 * 3600 ); %[rad/sec]
 Omega_m = [0,0,1]*omega_m; %[rad/sec]
 m = 10000; %[kg]
 d = 12; % [m] diameter heatshield
@@ -6,5 +8,5 @@ S = 12^2*pi/4; %[m^2]
 R_m = 6794000/2; %[m]
 h_atm = 104e3; %[m]
 z = [0 0 1]; %[-]
-
-%save('constants.mat');
+M_mars = 6.419*10^23; %[kg]
+G = 6.673*10^-11; %[N*(m/kg)^2]
