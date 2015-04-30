121c4e953f72aa808908432627f5cafc8ac80402
Björn <b.r.vandongen@gmail.com>
2015-04-30 09:56:48 +0200

    no message

--- SOFTWARE/orbitcheck.m
+++ SOFTWARE/orbitcheck.m
@@ -18,6 +18,8 @@ M_mars = 6.419*10^23; %[kg]
 G = 6.673*10^-11; %[N*(m/kg)^2]
 CLCD = 0.3; %[-]
 refinement_steps = 40;
+
+
 % changing variables
 
 rx = -4.15e6:-2e3:-4.2e6;
@@ -38,6 +40,32 @@ for k = 1:length(CD)
     rx_crashed = rx(1);
     for i=1:length(rx)
         [out, R, V, A] = orbitmodel_new(rx(i),ry,R_m,m,CD(k),S,v,dt,h_atmos,M_mars,G,CLCD);
+         t = 0:dt:(length(R)*dt-dt);
+                    subplot(3,1,1)
+                    hold on
+                    grid on
+                    plot(t,sqrt(R(:,1).^2 + R(:,2).^2 + R(:,3).^2),'color',cc(k,:));
+                    subplot(3,1,2)
+                    hold on
+                    grid on
+                    plot(t,sqrt(V(:,1).^2 + V(:,2).^2 + V(:,3).^2),'color',cc(k,:));
+                    subplot(3,1,3)
+                    hold on
+                    grid on
+                    plot(t,sqrt(A(:,1).^2 + A(:,2).^2 + A(:,3).^2),'color',cc(k,:));
+        
+                    %circle plot:
+                    theta_plot = 0:0.01:2*pi;
+                    radius_mars = ones(1,length(theta_plot)) * R_m;
+                    radius_mars_atmos = ones(1,length(theta_plot)) * (R_m + 104000);
+                    figure('name','Orbit')
+                    grid on
+                    axis equal
+                    hold on
+                    plot(R(:,1),R(:,2))
+                    polar(theta_plot,radius_mars,'r');
+                    polar(theta_plot,radius_mars_atmos,'g')
+                    
         if write_to_file
             fprintf(fid,'%11.1f %4.2f %d %d %d %f \n',rx(i),CD(k),out.inatmos,out.crash,out.inorbit,out.maxaccel);
         end
