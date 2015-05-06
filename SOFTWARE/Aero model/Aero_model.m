22e891372d36a2a2eea2727dc50771b0b501d74f
spcvanschie <sebastiaan.van.schie@live.nl>
2015-05-06 16:32:33 +0200

    Aero model begin

--- SOFTWARE/Aero model/Aero_model.m
+++ SOFTWARE/Aero model/Aero_model.m
@@ -1,4 +1,15 @@
+V_inf=[0,0,3000];
 R    = 5;
-r    = 2;
+r    = 3;
 theta= linspace(0,2*pi,100);
 phi  = linspace(0,2*pi,100);
+
+[theta,phi]=meshgrid(theta,phi);
+x=(R+r*cos(theta)).*cos(phi);
+y=(R+r*cos(theta)).*sin(phi);
+z=r.*sin(theta);
+kleurtjes = z;
+
+surface=surf(x,y,z, kleurtjes);
+
+axis equal
