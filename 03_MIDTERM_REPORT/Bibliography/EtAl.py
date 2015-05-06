c745d4d8be61a6f495fee705e57210ef9fa534bb
apvanoostrum <a.p.vanoostrum@gmail.com>
2015-05-06 17:05:25 +0200

    no message

---
+++ 03_MIDTERM_REPORT/Bibliography/EtAl.py
@@ -0,0 +1,26 @@
+#Short author list. --> alternative replace for mid term report
+
+
+n=open("library.bib", "r")
+bib=n.readlines()
+n.close()
+
+nr=4
+
+for i in range(len(bib)):
+    if  bib[i].find('author =')==0:
+        l=bib[i]
+        authors=l.split(' and')
+        if len(authors)>nr:
+            l=''
+            for j in range(nr-1):
+                l= l +authors[j] +' and '
+            l=l +'{ Others}} \n,'
+            bib[i]=l
+            
+
+f=open("Bibliography.bib", 'w')
+for i in range(len(bib)):
+    f.write(bib[i])
+f.close
+
