96fab17adbd936a3a68046ebc818f57a554b5b55
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2015-05-07 17:55:13 +0200

    added force calculation

--- SOFTWARE/Aero model/modnewtonian.m
+++ SOFTWARE/Aero model/modnewtonian.m
@@ -18,6 +18,7 @@ classdef modnewtonian
         % Calculated properties
         normals;
         cellcenters;
+        areas;
         Cpmax_array;
         Cpdist_array;
         CR_array;
@@ -25,6 +26,17 @@ classdef modnewtonian
     end
     
     methods
+%         function obj = modnewtonian(TriGeom, gamma, a, center)
+%             % Constructor: Set the geometry and gamma on Mars
+%             obj.coords = TriGeom.Points';
+%             obj.tri = TriGeom.ConnectivityList;
+%             obj.gamma = gamma;
+%             obj.a = a;
+%             obj.center = center;
+%             obj.normals = obj.calcsurfacenormals();
+%             obj.cellcenters = obj.calccellcenters();
+%         end
+        
         function obj = modnewtonian(coords, tri, gamma, a, center)
             % Constructor: Set the geometry and gamma on Mars
             obj.coords = coords;
@@ -34,24 +46,28 @@ classdef modnewtonian
             obj.center = center;
             obj.normals = obj.calcsurfacenormals();
             obj.cellcenters = obj.calccellcenters();
-        end
+        end        
         
         function obj = calcAero(obj, V)
             % Calculate aerodynamic properties for the geometry for given V
             obj.M_array = [obj.M_array, sqrt(sum(V.^2))/obj.a];
-            obj.Cpmax_array = [obj.Cpmax_array, obj.calcCp_max(obj.M_array(end))];
+            obj.Cpmax_array = [obj.Cpmax_array, calcCp_max(obj.M_array(end), obj.gamma)];
             obj.V_array = [obj.V_array, V];
             obj.Cpdist_array = [obj.Cpdist_array, obj.calcCpdist()];
-            CR = [0,0,0];
+            obj.CR_array = [obj.CR_array, obj.calcForceCoeffs()];
+            CR = obj.CR_array(end)
             CM = [0,0,0];
-            % Calculate!
-            
         end
         
         function obj = calcAeroangle(obj, Vinf, alpha, beta)
             % Calculate aerodynamic properties with aoa and sideslip and V
-            V = obj.transformation(alpha, beta)*[Vinf;0;0];
+            V = transformation(alpha, beta)*[Vinf;0;0];
             obj = obj.calcAero(V);
+        end
+        
+        function CR = calcForceCoeffs(obj)
+            % Calculate aerodynamic force coefficients on the body
+            CR = - obj.normals * (obj.Cpdist_array(:,end) .* obj.areas);
         end
         
         function Cpdist = calcCpdist(obj)
@@ -59,14 +75,10 @@ classdef modnewtonian
             Cpdist = zeros(size(obj.tri,1),1);
             Vhat = obj.V_array(:,end)/norm(obj.V_array(:,end));
             sinthetas = sum(obj.normals' * Vhat,2);
+            sinthetas(sinthetas<0) = 0;
             Cpdist = obj.Cpmax_array(end)*sinthetas.^2;
         end
         
-        function Cp_max = calcCp_max(obj, M)
-            % Get the Cp_max as needed for modified newtonian theory
-            gamma = obj.gamma; %To make the calculation shorter
-            Cp_max = 2./(gamma*M.^2).*((((gamma+1)^2*M.^2)/(4*gamma*M.^2-2*(gamma-1)))^(gamma/(gamma-1))*((1-gamma+2*gamma*M.^2)/(gamma+1))-1);
-        end
         
         function SN = calcsurfacenormals(obj)
             %Calculate the surface normals of the given geometry.
@@ -74,34 +86,37 @@ classdef modnewtonian
             for i = 1:size(obj.tri,1)
                 vec_a = obj.coords(:,obj.tri(i,2)) - obj.coords(:,obj.tri(i,1));
                 vec_b = obj.coords(:,obj.tri(i,3)) - obj.coords(:,obj.tri(i,1));
-                SN(:,i) = cross(vec_a, vec_b)/norm(cross(vec_a, vec_b));
+                SN(:,i) = -cross(vec_a, vec_b)/norm(cross(vec_a, vec_b));
             end
         end
         
         function centers = calccellcenters(obj)
+            % Calculate the cell centers of the given geometry
             centers = zeros(size(obj.tri'));
             for i = 1:size(obj.tri,1)
                 vectors = [obj.coords(:,obj.tri(i,1)),obj.coords(:,obj.tri(i,2)), obj.coords(:,obj.tri(i,3))];
                 centers(:,i) = mean(vectors,2);
             end
         end
-        
-        function T = transformation(obj, alpha, beta)
-            % Get the transformation matrix for angle of attack
-            Ty = [[cos(-alpha), 0,          -sin(-alpha)];
-                  [0,           1,          0           ];
-                  [sin(-alpha), 0,          cos(-alpha) ]];
-              
-            Tz = [[cos(beta),   sin(beta),  0           ];
-                  [-sin(beta),  cos(beta),  0           ];
-                  [0,           0,          1           ]];
-            T = Tz*Ty;
-        end
-        
-        
-        
-        
-        
+
     end
     
 end
+
+function T = transformation(alpha, beta)
+    % Get the transformation matrix for angle of attack
+    Ty = [[cos(-alpha), 0,          -sin(-alpha)];
+          [0,           1,          0           ];
+          [sin(-alpha), 0,          cos(-alpha) ]];
+
+    Tz = [[cos(beta),   sin(beta),  0           ];
+          [-sin(beta),  cos(beta),  0           ];
+          [0,           0,          1           ]];
+    T = Tz*Ty;
+end
+
+function Cp_max = calcCp_max(M, gamma)
+    % Get the Cp_max as needed for modified newtonian theory
+    Cp_max = 2./(gamma*M.^2).*((((gamma+1)^2*M.^2)/(4*gamma*M.^2-2*(gamma-1)))^(gamma/(gamma-1))*((1-gamma+2*gamma*M.^2)/(gamma+1))-1);
+end
+        
--- SOFTWARE/Aero model/modnewtoniantest.m
+++ SOFTWARE/Aero model/modnewtoniantest.m
@@ -1,4 +1,4 @@
-clear
+
 x = [0,1,0,0,0,-1];
 y = [0,0,1,0,-1,-1];
 z = [0,0,0,1,0,1];
@@ -8,6 +8,14 @@ a = 300;
 gamma = 1.4;
 center = zeros(3,1);
 
-mod = modnewtonian( coords, tri, gamma, a, center);
-mod = mod.calcAeroangle(7000,0,0);
-mod.Cpdist_array
+% mod = modnewtonian( coords, tri, gamma, a, center);
+mod = modnewtonian( TriGeom.Points', TriGeom.ConnectivityList, gamma, a, center);
+mod = mod.calcAeroangle(70000000,deg2rad(45),0);
+mod.Cpdist_array;
+
+trisurf(Tri,xvector,yvector, zvector, mod.Cpdist_array);
+axis equal;
+colorbar;
+xlabel('x')
+ylabel('y')
+zlabel('z')
