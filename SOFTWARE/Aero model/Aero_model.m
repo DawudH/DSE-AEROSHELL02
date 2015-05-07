9ee659c035788d93421a792c2175ca5c3bbf415b
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2015-05-07 12:32:00 +0200

    tritest

--- SOFTWARE/Aero model/Aero_model.m
+++ SOFTWARE/Aero model/Aero_model.m
@@ -1,8 +1,8 @@
 V_inf=[0,0,3000];
 R    = 5;
 r    = 3;
-theta= linspace(0,2*pi,100);
-phi  = linspace(0,2*pi,100);
+theta= linspace(0,2*pi,10);
+phi  = linspace(0,2*pi,10);
 
 [theta,phi]=meshgrid(theta,phi);
 x=(R+r*cos(theta)).*cos(phi);
@@ -25,7 +25,7 @@ for n = 1:length(x(1,:))
     end
 end
 
-dt = DelaunayTri(xvector', yvector', zvector');
+dt = delaunayTriangulation(xvector', yvector', zvector');
 [tri,Xb] = freeBoundary(dt);
 figure;
 trisurf(tri,Xb(:,1),Xb(:,2),Xb(:,3), 'FaceColor', 'cyan', 'faceAlpha', 0.8);
