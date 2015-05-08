35d7f2bdba89d2c4f56b91beddcdd0fa40a24ec2
Twan Keijzer <twankeijzer@hetnet.nl>
2015-05-08 16:22:24 +0200

    small_change

--- SOFTWARE/orbital_model/compute_orbit.m
+++ SOFTWARE/orbital_model/compute_orbit.m
@@ -6,13 +6,13 @@ close all
 constants
 
 ry = 10* R_m; %[m]
-v = 5000; %[m/s]
+v = 6000; %[m/s]
 dt_init = 1;
 dt_atmos = 1;
 dt_kep_init = 1e-8;
 CD = 1.25;
-rx = -4705000;
-CL = 0.63;
+rx = -4386666.666667;
+CL = -0.187500;
 tend = 3600 * 24* 10;
 
 [out] = orbit_full(rx,ry,CD,v,dt_init,dt_atmos,dt_kep_init,CL,tend);
