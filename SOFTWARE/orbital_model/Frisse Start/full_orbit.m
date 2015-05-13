559caf5099eb67e77c97b50b8030996fe1106ea1
DawudH <d.d.hage@student.tudelft.nl>
2015-05-13 12:07:31 +0200

    no message

--- SOFTWARE/orbital_model/Frisse Start/full_orbit.m
+++ SOFTWARE/orbital_model/Frisse Start/full_orbit.m
@@ -17,6 +17,7 @@ function [ out ] = full_orbit(R0, V0, A0, G, M_mars, R_m, h_atm, atm, dt_kep_ini
     speed_sound(1,:) = out_hk.end.speed_sound;
     V(1,:) = out_hk.end.V;
     M(1,:) = out_hk.end.M;
+    speed_sound(1,:) = out_hk.end.speed_sound;
     A(1,:) = out_hk.end.A;
     Ag(1,:) = out_hk.end.Ag;
     Ad(1,:) = out_hk.end.Ad;
@@ -64,6 +65,7 @@ function [ out ] = full_orbit(R0, V0, A0, G, M_mars, R_m, h_atm, atm, dt_kep_ini
         R(i+1,:) = out_o.R;
         speed_sound(i+1,:) = out_o.speed_sound;
         V(i+1,:) = out_o.V;
+        speed_sound(i+1,:) = out_o.speed_sound;
         M(i+1,:) = out_o.M;
         A(i+1,:) = out_o.A;
         Ag(i+1,:) = out_o.Ag;
@@ -100,11 +102,12 @@ function [ out ] = full_orbit(R0, V0, A0, G, M_mars, R_m, h_atm, atm, dt_kep_ini
     out.a = out_hk.param.a;
     out.e = out_hk.param.e;
     out.c = out_c;
+    out.speed_sound = speed_sound;
     
     
     % output text
     a_human_mag = sqrt((out.Ad(:,1)+out.Al(:,1)).^2 + (out.Ad(:,2)+out.Al(:,2)).^2 + (out.Ad(:,3)+out.Al(:,3)).^2);
     maxaccel = max(a_human_mag)/g_earth;
-    disp(['rx = ' num2str(rx) ' [m], CD = ' num2str(abs(control.CL_init) / control.CLCD) ' [-], CL = ' num2str(control.CL_init) ' [-], in atmosphere: ' num2str(out_c.in_atmos) ', crashed: ' num2str(out_c.crash) ', in orbit: ' num2str(out_c.inorbit) ', acceleration: ' num2str(maxaccel) ', time pased: ' num2str(t/(3600*24)) ' days' ])
+    disp(['rx = ' num2str(R0(1)) ' [m], CD = ' num2str(abs(control.CL_init) / control.CLCD) ' [-], CL = ' num2str(control.CL_init) ' [-], in atmosphere: ' num2str(out_c.in_atmos) ', crashed: ' num2str(out_c.crash) ', in orbit: ' num2str(out_c.inorbit) ', acceleration: ' num2str(maxaccel) ', time pased: ' num2str(t/(3600*24)) ' days' ])
 end
 
