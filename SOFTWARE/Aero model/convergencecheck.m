87cdd29a028efd87e2f1677942a8c1fee37a5974
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2015-05-19 01:47:52 +0200

    2 more heatfluxes

--- SOFTWARE/Aero model/convergencecheck.m
+++ SOFTWARE/Aero model/convergencecheck.m
@@ -1,4 +1,5 @@
 % close all;
+
 shapetexts.horizontalplate = 'horizontalplate';
 shapetexts.verticalplate = 'verticalplate';
 shapetexts.sphere12m = 'sphere12m';
@@ -7,45 +8,34 @@ shapetexts.deg60cone = 'deg60cone';
 shapetexts.deg30cone = 'deg30cone';
 shapetexts.irvevalidation = 'irvevalidation';
 shapetexts.apollovalidation = 'apollovalidation';
+shapetexts.torus = 'torus';
+shapetexts.concept_irve = 'concept_irve';
+shapetexts.concept_apollo = 'concept_apollo';
+shapetexts.concept_isotensoid = 'concept_isotensoid';
 
 a = 300;
 gamma = 1.4;
 center = zeros(3,1);
 rho = 1e-3;
 T = 150;
-q = 10;
-
-alpha0 = 0; %degrees
-dalpha = 1; %degrees
-alphaend = 15; %degrees
-qarray = [6:2:60, 65:5:100];
-CD = [];
+qarray = [5:2:40];
+qmax = [];
 
 for q = qarray
     disp(strcat('Current q:', num2str(q)));
-    [ coords, tri, A ] = generategeometry( shapetexts.pastille12m15m, q );
-
+    [ coords, tri, A ] = generategeometry( shapetexts.irvevalidation, q );
     
     mod = modnewtonian( coords, tri, gamma, a, center, rho, T, A);
-    % mod = mod.alphasweep(7e3, 0, deg2rad(alpha0), deg2rad(alphaend), deg2rad(dalpha));
 
     mod = mod.calcAeroangle(7e3,deg2rad(1),0);
-    CD = [CD, mod.CR_aero_array(1,1)];
+    qmax(end+1) = mod.qmax_array(1,1);
 end
-CD;
-    % mod.plotCp(true, true);
-    % mod.CR_aero_array
-figure;
-plot(qarray, CD);
-% mod.plots(rad2deg(mod.alpha_array), 'alpha', {{'cl'}, {'cd'}, {'clcd'}, {'cx'}, {'cz'},{'cmy'},{'q'}, {'T'}});
 
-ratio = CD/CD(end);
-bestarray = find(ratio>0.999);
+figure;
+plot(qarray, qmax);
+
+ratio = qmax/qmax(end);
+bestarray = find((1.001>ratio) & (ratio>0.999));
 disp(strcat('q required for 99.9% convergence: ', num2str(qarray(bestarray(1)))));
-bestarray = find(ratio>0.995);
-disp(strcat('q required for 99.5% convergence: ', num2str(qarray(bestarray(1)))));
-% for i = 2:length(CD)
-% i;
-% CD(i)/CD(end);
-% end
-    
+bestarray = find((1.005>ratio) & (ratio>0.995));
+disp(strcat('q required for 99.5% convergence: ', num2str(qarray(bestarray(1)))));  
