247e113fac8511cdb7bb071cf28ad1b1ecb54e7e
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2015-05-19 16:43:02 +0200

    symbols fixed

--- SOFTWARE/aerodynamic_coefficients/plotaeroplots.m
+++ SOFTWARE/aerodynamic_coefficients/plotaeroplots.m
@@ -35,5 +35,35 @@ plot(rad2deg(alpha), aerapollo.getCMYA(alpha));
 plot(rad2deg(alpha), aerisotensoid.getCMYA(alpha));
 plot(rad2deg(alpha), aertorus.getCMYA(alpha));
 xlabel('\alpha (deg)');
-ylabel('C_{M_Y}A (m^2)');
+ylabel('C_{M}A (m^2)');
+legend('Stacked Toroid, Tension Cone', 'Rigid', 'Isotensoid', 'Trailing Ballute');
+
+figure;
+hold on;
+plot(rad2deg(alpha), aerirve.getLiftGradient(alpha));
+plot(rad2deg(alpha), aerapollo.getLiftGradient(alpha));
+plot(rad2deg(alpha), aerisotensoid.getLiftGradient(alpha));
+plot(rad2deg(alpha), aertorus.getLiftGradient(alpha));
+xlabel('\alpha (deg)');
+ylabel('C_{L_\alpha}A (m^2)');
+legend('Stacked Toroid, Tension Cone', 'Rigid', 'Isotensoid', 'Trailing Ballute');
+
+figure;
+hold on;
+plot(rad2deg(alpha), aerirve.getDragGradient(alpha));
+plot(rad2deg(alpha), aerapollo.getDragGradient(alpha));
+plot(rad2deg(alpha), aerisotensoid.getDragGradient(alpha));
+plot(rad2deg(alpha), aertorus.getDragGradient(alpha));
+xlabel('\alpha (deg)');
+ylabel('C_{D_\alpha}A (m^2)');
+legend('Stacked Toroid, Tension Cone', 'Rigid', 'Isotensoid', 'Trailing Ballute');
+
+figure;
+hold on;
+plot(rad2deg(alpha), aerirve.getMomentGradient(alpha));
+plot(rad2deg(alpha), aerapollo.getMomentGradient(alpha));
+plot(rad2deg(alpha), aerisotensoid.getMomentGradient(alpha));
+plot(rad2deg(alpha), aertorus.getMomentGradient(alpha));
+xlabel('\alpha (deg)');
+ylabel('C_{M_\alpha}A (m^2)');
 legend('Stacked Toroid, Tension Cone', 'Rigid', 'Isotensoid', 'Trailing Ballute');
