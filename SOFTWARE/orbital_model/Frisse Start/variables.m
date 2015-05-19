1a5d026d635252560e34690430e890305aa7b790
DawudH <d.d.hage@student.tudelft.nl>
2015-05-19 10:50:32 +0200

    no message

--- SOFTWARE/orbital_model/Frisse Start/variables.m
+++ SOFTWARE/orbital_model/Frisse Start/variables.m
@@ -5,7 +5,7 @@ added_paths
 constants
 
 %Initial Position
-rx = -4140000;
+rx = -4142343.75;
 ry = 10*R_m;
 R = [rx,ry,0];
 
@@ -17,21 +17,21 @@ V = [0,-v,0];
 A = G*M_mars/norm(R(1,:))^3*R(1,:);
 
 %timesteps
-dt_atmos = 0.1; %[s]
+dt_atmos = 0.5; %[s]
 dt_kep_init = 1e-6; %[s]
 
 %time to end simulation
-tend = 3600 * 24 * 4; %[s]
+tend = 3600 * 24 * 600; %[s]
 
 %Control variables
 control.a = 2.5*g_earth;
 control.dalphadt = 1*pi/180;
 control.dalpha = control.dalphadt*dt_atmos;
-control.alpha_init = -5*pi/180; % rad
+control.alpha_init = -25*pi/180; % rad
 control.alpha_range = [-60 60]*pi/180;
 
 % create atmosphere object
 atm = marsatmosphere();
 % create aerocoef object
-aero_coef = aeroProperties('irve');
+aero_coef = aeroProperties('apollo');
 
