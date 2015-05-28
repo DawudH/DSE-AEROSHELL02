5e4565cc33c57acff16968787598626c3595ad37
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2015-05-28 17:05:33 +0200

    extracted all geometry code to aeroGeometry code

---
+++ SOFTWARE/Aero model/aeroGeometry.m
@@ -0,0 +1,211 @@
+classdef aeroGeometry
+% A definition of a geometry as used by modnewtonian, including helper functions
+
+    properties
+        %Geometry definition
+        TriGeom;
+        coords;
+        tri;
+        A_frontal;
+        
+        %Calculated properties
+        normals;
+        areas;
+        centers;
+        
+    end
+    
+    methods
+        function obj = aeroGeometry(TriGeom, A_frontal)
+            obj.TriGeom = TriGeom;
+            obj.A_frontal = A_frontal;
+            obj.tri = TriGeom.ConnectivityList;
+            obj.coords = TriGeom.Points';
+            [obj.normals, obj.areas] = obj.calcNormalsAreas(obj.tri, obj.coords);
+            obj.centers = obj.calcCellCenters(obj.tri, obj.coords);
+        end
+        
+        function [normals, areas] = calcNormalsAreas(~, tri, coords)
+            %Calculate the surface normals of the given geometry.
+            normals = zeros(size(tri'));
+            areas = zeros(size(tri,1),1);
+            for i = 1:size(tri,1)
+                vec_a = coords(:,tri(i,2)) - coords(:,tri(i,1));
+                vec_b = coords(:,tri(i,3)) - coords(:,tri(i,1));
+                normals(:,i) = cross(vec_a, vec_b)/norm(cross(vec_a, vec_b));
+                areas(i) = 0.5*norm(cross(vec_a, vec_b));
+            end
+        end
+        
+        function centers = calcCellCenters(~, tri, coords)
+            % Calculate the cell centers of the given geometry
+            centers = zeros(size(tri'));
+            for i = 1:size(tri,1)
+                vectors = [coords(:,tri(i,1)),coords(:,tri(i,2)), coords(:,tri(i,3))];
+                centers(:,i) = mean(vectors,2);
+            end
+        end
+        
+        function oppositePoint = getOpposite(obj, tbase, side)
+            adjacents = obj.getAdjacentTriangles(tbase);
+            temp = [3,1,2];
+            for i = 1:3
+                [baseside, checkside] = adjacency(obj, tbase, adjacents(i));
+                if baseside == side
+                    oppositePoint = obj.tri(adjacents(i),temp(checkside));
+                    break;
+                end
+            end
+        end
+        
+        function [baseside, checkside] = getAdjacency(obj, tbase, tcheck)
+            % Base is the central triangle, check is the triangle of which
+            % you want to know the connection
+            combischeck = obj.tri(tcheck, :);
+            combischeck = [combischeck(1),combischeck(2);combischeck(2),combischeck(3);combischeck(3),combischeck(1)];
+            combisbase = obj.tri(tbase, :);
+            combisbase = [combisbase(2),combisbase(1);combisbase(3),combisbase(2);combisbase(1),combisbase(3)];
+            baseside = 0;
+            checkside = 0;
+            for basesidecount = 1:3
+                for checksidecount = 1:3
+                    if sum(combischeck(checksidecount,:)==combisbase(basesidecount,:))==2
+                        baseside = basesidecount;
+                        checkside = checksidecount;
+                    end
+                end
+            end
+        end
+        
+        function ind = getTriangle(obj, n1, n2, n3)
+            % This function is not used as of now
+            v = [n1; n2; n3];
+            p = perms(v);
+            ind = 0;
+            for i = 1:size(p,1)
+                equalrows = sum(obj.tri == ones(size(obj.tri)) * diag(p(i,:)), 2);
+                if length(find(equalrows==3)) == 1
+                    ind = find(equalrows==3);
+                end
+            end
+        end
+        
+        function r = calcRadiusOfCurvature(obj, n1, n2, n3)
+            A = obj.coords(:,n1);
+            B = obj.coords(:,n2);
+            C = obj.coords(:,n3);
+            a = A-C;
+            b = B-C;
+            r = (norm(a)*norm(b)*norm(a-b))/(2*norm(cross(a,b)));
+        end
+        
+        function indices = getAdjacentTriangles(obj, triangle)
+            T = triangulation(obj.tri, obj.coords');
+            indices = neighbors(T, triangle);
+        end
+        
+        function points = getPointsOnXZPlane(obj, y)
+            epsilon = 1e-10;
+            points = find(obj.coords(2,:)>y-epsilon & obj.coords(2,:)<y+epsilon);
+        end
+        
+        function points = getPointsOnYZPlane(obj, x)
+            epsilon = 1e-10;            
+            points = find(obj.coords(1,:)>x-epsilon & obj.coords(1,:)<x+epsilon);
+        end
+        
+        function points = getPointsOnXYPlane(obj, x)
+            epsilon = 1e-10;            
+            points = find(obj.coords(3,:)>x-epsilon & obj.coords(3,:)<x+epsilon);
+        end    
+        
+        function unconnectedTriangles = getUnconnectedTriangles(obj)
+            unconnectedTriangles = [];
+            for i = 1:size(obj.tri,1)
+                if sum(isnan(obj.getAdjacentTriangles(i))) > 0
+                    unconnectedTriangles(end+1) = i;
+                end
+            end
+        end
+        
+        function [] = plotUnconnectedTriangles(obj, pauselength)
+            unc = obj.getUnconnectedTriangles();
+            for i = 1:length(unc)
+                obj.plotTriangle(unc(i));
+                pause(pauselength);
+            end
+        end
+        
+        function [] = plotPoint(obj, n)
+        %PLOTPOINT Plot a point on the 3d plot
+            plot3(obj.coords(1,n), obj.coords(2,n), obj.coords(3,n), 'o');
+        end
+        
+        function [] = plotTriangle(obj, n)
+        %PLOTTRIANGLE Plot the triangle
+            t = obj.tri(n,:);
+            obj.plotPoint(t(1));
+            obj.plotPoint(t(2));
+            obj.plotPoint(t(3));
+        end     
+   
+        function triangles = getTrianglesOnPoint(obj, point)
+            triangles = [find(obj.tri(:,1)==point);find(obj.tri(:,2)==point);find(obj.tri(:,3)==point)];
+        end        
+      
+        
+        function f = plotGeometry(obj, plotfaces, plotnormals)
+            f = figure;
+            h1 = axes;
+            set(h1, 'Zdir', 'reverse');            
+            hold on;
+            caxis([0,1]);
+            if plotfaces
+                trisurf(obj.tri,obj.coords(1,:),obj.coords(2,:),obj.coords(3,:), rand(size(obj.tri(:,1))), 'EdgeColor', 'none');
+            end
+            axis equal;
+            xlabel('x')
+            ylabel('y')
+            zlabel('z')
+            if plotnormals
+                quiver3(obj.centers(1,:), obj.centers(2,:), obj.centers(3,:), obj.normals(1,:), obj.normals(2,:), obj.normals(3,:))
+            end
+            zlim([min(obj.coords(3,:)), max(obj.coords(3,:))]);
+            ylim([min(obj.coords(2,:)), max(obj.coords(2,:))]);
+            view(40,30);
+        end
+        
+        
+        function f = plotValues(obj, values, variablename, colorrange, plotfaces, plotnormals)
+            f = figure;
+            h1 = axes;
+            set(h1, 'Zdir', 'reverse');            
+            hold on;
+            if plotfaces
+                trisurf(obj.tri,obj.coords(1,:),obj.coords(2,:),obj.coords(3,:), values, 'EdgeColor', 'none');
+            end
+            caxis(colorrange); %Cp goes from 0 to 2
+            axis equal;
+            h = colorbar;
+            ylabel(h, variablename, 'interpreter', 'latex');
+            xlabel('x')
+            ylabel('y')
+            zlabel('z')
+            if plotnormals
+                quiver3(obj.centers(1,:), obj.centers(2,:), obj.centers(3,:), obj.normals(1,:), obj.normals(2,:), obj.normals(3,:))
+            end
+            zlim([min(obj.coords(3,:)), max(obj.coords(3,:))]);
+            ylim([min(obj.coords(2,:)), max(obj.coords(2,:))]);
+            view(40,30);
+            %Activate following code to plot vector of the wind
+%             xlength = max(obj.coords(1,:))-min(obj.coords(1,:));
+%             quiverV = - xlength * 0.5 * obj.V_array(:,end) / norm(obj.V_array(:,end));
+%             quiverx = xlength*0.5 - quiverV(1) + max(obj.coords(1,:));
+%             quiver3(quiverx,mean(obj.coords(2,:))-quiverV(2),mean(obj.coords(3,:))-quiverV(2),quiverV(1), quiverV(2), quiverV(3));
+        end
+        
+    end
+    
+    
+    
+end
