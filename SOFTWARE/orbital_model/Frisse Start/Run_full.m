6bb969bc51669297eab5e5ca6d89f9f5d05d615f
DawudH <d.d.hage@student.tudelft.nl>
2015-05-29 10:17:22 +0200

    no message

--- SOFTWARE/orbital_model/Frisse Start/Run_full.m
+++ SOFTWARE/orbital_model/Frisse Start/Run_full.m
@@ -1,6 +1,6 @@
 clc
 clear
-%close all
+close all
 
 %%Input constants & variables
 variables
@@ -8,9 +8,10 @@ variables
 % booleans
 use_control = false;
 multiple_orbits = true;
+use_alpha_profile = true;
 
 %%function
-[out] = full_orbit(R, V, A, G, M_mars, R_m, h_atm, atm, dt_kep_init, dt_atmos, m, Omega_m, S, control, tend, crash_margin, g_earth, aero_coef, use_control, multiple_orbits);
+[out] = full_orbit(R, V, A, G, M_mars, R_m, h_atm, atm, dt_kep_init, dt_atmos, m, Omega_m, S, control, tend, crash_margin, g_earth, aero_coef, use_control, multiple_orbits, use_alpha_profile);
                     
 %% processing (plot/write to file)
 figure('name','parameters over time')
