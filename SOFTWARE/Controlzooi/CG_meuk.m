3f92eacdadcc1ca33f8fd416185c387da656f919
spcvanschie <sebastiaan.van.schie@live.nl>
2015-06-01 17:08:44 +0200

    no message

--- SOFTWARE/Controlzooi/CG_meuk.m
+++ SOFTWARE/Controlzooi/CG_meuk.m
@@ -1,10 +1,10 @@
 %% Definition of variables
 % Points with respect to axisymmetric surface center
-cg=[0.3,0,1.5]; % cg location
+cg=[0.3,0,3.]; % cg location
 flap_centroid=[5, 0, 6]; % flap geometrical centroid location
 
 % Control system limitations
-phi_diff_max=deg2rad(5); % C.G. rotation system maximum rotation angle
+phi_diff_max=deg2rad(10); % C.G. rotation system maximum rotation angle
 dalpha_dt=0.2; % deg/s
 
 % Relations between trim angle and C.G. offset, gebeund uit modnewtonian
@@ -27,9 +27,11 @@ CMY_A_trim_flaps=CMY_A_per_deg*alpha_trim_flaps; % CMY_A handled by flaps to tri
 %alpha_flaps_range=linspace(cg_alpha_trim_max,alpha_max,3);
 %CMY_A_flaps_range=CMY_A_per_deg*alpha_flaps_range;
 
+flap_offset=flap_centroid-cg;
+
 % Flap anglescg_alpha_trim_max
 %theta=60; % surface inclination in degrees, w.r.t. body-symmetric axis (body frame)
-theta_range=linspace(10,60,2);
+theta_range=linspace(10,60,4);
 
 q=2000; % Dynamic pressure
 gamma=1.4;
@@ -37,9 +39,8 @@ M=30;
 
 Cp_max = 2/(gamma*M^2)*((((gamma+1)^2*M^2)/(4*gamma*M^2-2*(gamma-1)))^(gamma/(gamma-1))*((1-gamma+2*gamma*M^2)/(gamma+1))-1);
 flap_normal=[-cos(deg2rad(theta_range+alpha_max))',zeros(size(theta_range))',-sin(deg2rad(theta_range+alpha_max))'];
-CR=-Cp_max*(sin(deg2rad(90+alpha_max+diag(theta_range))).^2)*flap_normal;
-%CR=-Cp_max*(sin(deg2rad(90+theta+alpha_flaps_range)).^2)*flap_normal;
+CR=flap_normal'*diag(-Cp_max*(sin(deg2rad(90+alpha_max+theta_range)).^2));
 
-CM_flap=cross(repmat((flap_centroid-cg)',1,length(theta_range)),CR'); % CM delivered per flap
+CM_flap=cross(repmat(flap_offset',1,length(theta_range)),CR); % CM delivered per flap
 
-plot(theta_range,CM_flap(2,:));
+plot(theta_range,CM_flap(2,:)');
