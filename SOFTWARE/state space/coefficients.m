503a4f0f53dd5618fb32694315aa5806cc19ee12
spcvanschie <sebastiaan.van.schie@live.nl>
2015-06-02 12:03:02 +0200

    no message

--- SOFTWARE/state space/coefficients.m
+++ SOFTWARE/state space/coefficients.m
@@ -9,6 +9,31 @@ agammaV
 enz...
 
 %%Sebastiaan
-agammagamma
-enz...
-    
+agammagamma = -((V0/R0)-(g0/V0))*sin(gamma0);
+agammaR = ((2*g0/V0)-(V0/R0))*cos(gamma0)/R0;
+agammaalpha = (cos(sigma0)/(m*V0))*CLalpha*q0*Sref;
+agammabeta=-(sin(sigma0)/(m*V0))*CSbeta*q0*Sref;
+agammasigma = -(L0/(m*V0))*sin(sigma0);
+agammap = 0;
+agammaq = 0;
+agammar = 0;
+aRV = sin(gamma0);
+aRgamma = V0*cos(gamma0);
+aRR = 0;
+aRp = 0;
+aRq = 0;
+aRr = 0;
+aRalpha = 0;
+aRbeta = 0;
+aRsigma = 0;
+apbeta = (1/Ixx)*q0*Sref*bref*CLbeta;
+apV = 0;
+apgamma = 0;
+apR = 0;
+app = 0;
+apq = 0;
+apr = 0;
+apalpha = 0;
+apsigma = 0;
+aqV=(M0/(Iyy*V0))*CmM*q0*Sref*cref;
+aqalpha=(1/Iyy)*Cmalpha*q0*Sref*cref;
