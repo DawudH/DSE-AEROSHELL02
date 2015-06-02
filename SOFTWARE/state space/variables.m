6d7a51cfcea1ed15fde155f92d0407c7d234ef9a
Twan Keijzer <twankeijzer@hetnet.nl>
2015-06-02 11:16:41 +0200

    veranderingen state2

--- SOFTWARE/state space/variables.m
+++ SOFTWARE/state space/variables.m
@@ -1,12 +1,19 @@
-%%constant
+%%%constant
 %%Sebastiaan
 
 
 %%Twan
 m = 10000; %[kg]
+Sref = pi/4*12^2; %[m^2]
 
-%%variable
+
+%%%variable
+%%Twan
 V0 = 7000; %[m/s]
 M0 = 50; %[-]
 rho0 = 1e-15; %[kg/m^3]
 q0 = rho0*V0^2/2; %[kg/(m*s^2)]
+CD0 = ??; %[-]
+D0 = CD0*q0*Sref;
+
+%%Sebastiaan
