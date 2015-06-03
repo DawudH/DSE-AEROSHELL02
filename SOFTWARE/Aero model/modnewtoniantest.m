3aa520304d0277e70ece9045f7a88c770245f1ac
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2015-06-03 10:44:58 +0200

    added comments

--- SOFTWARE/Aero model/modnewtoniantest.m
+++ SOFTWARE/Aero model/modnewtoniantest.m
@@ -1,25 +1,18 @@
 clear;
 
 
-a = 300;
-gamma = 1.4;
-rho = 1e-5;
-T = 150;
-q = 80;
-M = 40;
-V = a*M;
-beta = 0;
-alpha = 0;
-
-% alpha0 = -25; %degrees
-% dalpha = 5; %degrees
-% alphaend = 25; %degrees
+a = 150; %speed of sound
+gamma = 1.29; %gamma
+rho = 1e-5; %Density
+T = 150; %Temperatuure
+q = 80; % Maat voor aantal elementen
+M = 40; %Mach number
+V = a*M; %Velocity
+beta = 0; %Sideslip
+alpha = 0; %Angle of attack
 
 [ TriGeom, A, center ] = generategeometry( q );
 geom = aeroGeometry(TriGeom, A);
-% tic
-% geom.calcDistances(geom.coords);
-% toc
 
 mod = modnewtonian(geom, gamma, a, center, rho, T);
 mod = mod.calcAeroangle(V, alpha, beta);
