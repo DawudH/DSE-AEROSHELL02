60a29bd7bd3f09e177dd8f8c933761cf12eab592
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2015-06-03 11:01:15 +0200

    added bank angle

--- SOFTWARE/Aero model/modnewtoniantest.m
+++ SOFTWARE/Aero model/modnewtoniantest.m
@@ -5,19 +5,20 @@ a = 150; %speed of sound
 gamma = 1.29; %gamma
 rho = 1e-5; %Density
 T = 150; %Temperatuure
-q = 80; % Maat voor aantal elementen
+q = 30; % Maat voor aantal elementen
 M = 40; %Mach number
 V = a*M; %Velocity
 beta = 0; %Sideslip
 alpha = 0; %Angle of attack
+phi = 0;
 
 [ TriGeom, A, center ] = generategeometry( q );
 geom = aeroGeometry(TriGeom, A);
 
 mod = modnewtonian(geom, gamma, a, center, rho, T);
-mod = mod.calcAeroangle(V, alpha, beta);
-
-plot(mod.alpha_array, mod.CRA_aero_array)
+% mod = mod.calcAeroangle(V, alpha, beta, phi);
+mod = mod.alphasweep(V, beta, phi, 0,0.5*pi,0.1);
+plot(mod.alpha_array, mod.CRA_aero_array(1,:))
 
 
 
