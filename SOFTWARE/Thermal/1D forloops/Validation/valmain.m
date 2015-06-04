b4a77048a007f8ba9bf05a4ced9c766ef10766b7
Suthes <sutheshan2004@msn.com>
2015-06-04 17:14:34 +0200

    endofday

--- SOFTWARE/Thermal/1D forloops/Validation/valmain.m
+++ SOFTWARE/Thermal/1D forloops/Validation/valmain.m
@@ -9,7 +9,7 @@ clc
 %% Define input
 
 %lay-up (t[mm], k[w/m/K], rho[kg/m3], cp[J/kg/K])
-filename   = 'IRVE4.txt';
+filename   = 'test.txt';
 layupin    = dlmread(filename);
 % SI units
 layup      = zeros(size(layupin));
@@ -17,24 +17,17 @@ layup(:,1) = layupin(:,1)./1000;
 layup(:,2:5) = layupin(:,2:5);
 L = layup(:,1);
 
-%Asro input, Tinf
-
-
-%Aero input, qsdot
-load('heatflux.mat','T','t','qmax_array')
-qaero = qmax_array(1223:5602);
-Tatm  = T(1223:5602); 
-timeq = t(1223:5602)-t(1223);
-clear('T','t')
 
 % time and aeroheat
 %ttot = t(end);  % end time of the orbit [s]
-ttot = timeq(end);  % end time of the orbit [s]
+ttot = 600;  % end time of the orbit [s]
+tdur = 90;
 dt   = 0.1;    % time step, chooseable
+T0 = 293;
+q0 = 31000;
 nmax = int32(ttot/dt);  % number of time steps
 t = [0:double(nmax-1)]*dt;
-qs = interp1(timeq,qaero,[t,t(end)+dt])*10000;
-Tamb = interp1(timeq,Tatm,[t,t(end)+dt]);
+fact    = 1;           % multiplication factor of number of space steps.
 
 % spaceing
 L = int32(round(L*10000000));
@@ -55,7 +48,6 @@ L = double(L)/10000000;
 
 
 Ltot = sum(L);      % length of the layup [m]
-fact    = 1;           % multiplication factor of number of space steps.
 dx   = maxdx/fact;     % length of a space step
 imax =  int32(Ltot/dx + 1); % maximum amount of points in spaceing
 
@@ -87,8 +79,13 @@ v = alpha*dt/dx/dx;
 % i is space, n is time
 % space is rows, time is columns
 T = zeros(imax,nmax);
-T0 = 293;
 T(:,1) = T0;
+Tamb = ones(nmax,1)*T0;
+qs = ones(nmax,1)*q0;
+qs(tdur/dt:end) = 0;
+qrfac = zeros(nmax,1);
+qrfac(tdur/dt:end) = 1;
+
 
 w = zeros(imax,1);
 w(1:end-1) = v/2;
@@ -102,28 +99,30 @@ Cinv = inv(sparse(C));
 G = full(Cinv*sparse(N));
 
 for n=1:nmax-1
-    qr = -emiss*sig*(T(1,n)^4-Tamb(n)^4);
+    qr = -emiss*sig*(T(1,n)^4-Tamb(n)^4)*1;
     A(1) = ((qs(n)+qr)/k(1))*v(1)*dx;
     H = full(Cinv*sparse(A));
     T(:,n+1) = G*T(:,n) + H;
 end
 
 %% Contour-Plot
-% t = [0:double(nmax-1)]*dt;
-x = [0:double(imax-1)]*dx;
-figure;
-hold on
-contourf(t,x,T);
-colormap parula
-colorbar
-% title('Temperature over time and distance','Interpreter','LaTex','FontSize',13)
-% xlabel('Time [s]','Interpreter','LaTex','FontSize',14)
-% ylabel('Depth [m]','Interpreter','LaTex','FontSize',14)
+contourplot = 0;
+if contourplot
+    % t = [0:double(nmax-1)]*dt;
+    x = [0:double(imax-1)]*dx;
+    figure;
+    hold on
+    contourf(t,x,T);
+    colormap parula
+    colorbar
+    % title('Temperature over time and distance','Interpreter','LaTex','FontSize',13)
+    % xlabel('Time [s]','Interpreter','LaTex','FontSize',14)
+    % ylabel('Depth [m]','Interpreter','LaTex','FontSize',14)
 
-for j = 2:length(indexx)-1
-    plot([0,nmax*dt],[indexx(j)*dx,indexx(j)*dx],'--','Color', [255.0/256.0,130.0/256.0,28.0/256.0])
+    for j = 2:length(indexx)-1
+        plot([0,nmax*dt],[indexx(j)*dx,indexx(j)*dx],'--','Color', [255.0/256.0,130.0/256.0,28.0/256.0])
+    end
 end
-
 %% Layer analysis output
 results = zeros(length(indexx)-1,1);
 layernames = cell(length(indexx)-1,1);
@@ -133,7 +132,20 @@ for j=1:length(indexx)-1
 end
 output = table(L*1000,results,layup(:,2),layup(:,3),layup(:,4),'RowNames',layernames,'VariableNames',{'Thickness','maxT','k','rho','cp'});
     
-    
+%% Validation
+valid = 1;
+if valid
+    valres = dlmread('layup1res.txt');
+    figure;
+    hold on
+    plot(valres(:,1),valres(:,2),'b-')
+    plot(valres(:,1),valres(:,4),'r-')
+%     for j = 2:length(valres(1,:))
+%         plot(valres(:,1),valres(:,j))
+%     end
+    plot(t,T(1,:),'b--')
+    plot(t,T(16,:),'r--')
+end
     
     
     
