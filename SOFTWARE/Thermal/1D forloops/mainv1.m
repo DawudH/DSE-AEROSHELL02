f8e4728871aad77b9f9b4387bdeb2e2645976cff
Suthes <sutheshan2004@msn.com>
2015-06-08 16:41:45 +0200

    hermo

--- SOFTWARE/Thermal/1D forloops/mainv1.m
+++ SOFTWARE/Thermal/1D forloops/mainv1.m
@@ -1,5 +1,6 @@
 %% Thermal tool FR
 % Suthes & Lucas
+% v1 - Implement contact resistance
 %% Log:
 % 2-6 10:48 -- Validated correctly with 1-layer copper
 clear all
@@ -9,26 +10,34 @@ clc
 %% Define input
 
 %lay-up (t[mm], k[w/m/K], rho[kg/m3], cp[J/kg/K])
-filename   = 'layup1.txt';
+filename   = 'layup3.txt';
 layupin    = dlmread(filename);
 % SI units
 layup      = zeros(size(layupin));
 layup(:,1) = layupin(:,1)./1000;
-layup(:,2:5) = layupin(:,2:5);
+layup(:,2:6) = layupin(:,2:6);
 L = layup(:,1);
+
+% Aero input, qsdot
+load('heatflux.mat','T','t','qmax_array')
+tq = find(not(qmax_array==0));
+qaero = qmax_array(tq(1):tq(end));
+Tatm  = T(tq(1):tq(end)); 
+timeq = t(tq(1):tq(end))-t(tq(1));
+clear('T','t')
 
 
 % time and aeroheat
 %ttot = t(end);  % end time of the orbit [s]
-ttot = 600;  % end time of the orbit [s]
-tdur = 90;
+ttot = timeq(end);  % end time of the orbit [s]
+% tdur = 90;
 dt   = 0.1;    % time step, chooseable
 T0 = 293;
-q0 = 31000;
+q0 = qaero*10000;
 nmax = int32(ttot/dt);  % number of time steps
 t = [0:double(nmax-1)]*dt;
 fact    = 1;           % multiplication factor of number of space steps.
-kfact = [5e-6/fact;2.5e-1/fact;2.5e-1/fact];
+kfact = [2.5e-4;2.5e-4;2.5e-5;2.5e-5;2.5e-5];
 
 
 % spaceing
@@ -58,97 +67,59 @@ emiss = layup(1,5);
 sig   = 5.670373e-8; %[W/m2/K4]
 
 %% Material properties
-voidlayers = length(L)-1;
-kvoid = zeros(imax+voidlayers-1,1);
-rhovoid = zeros(imax+voidlayers-1,1);
-cpvoid = zeros(imax+voidlayers-1,1);
-k     = zeros(imax-1,1); 
-rho   = zeros(imax-1,1);
-cp    = zeros(imax-1,1);
+layers = length(L)-1;
+k = zeros(imax+layers-1,1);
+rho = zeros(imax+layers-1,1);
+cp = zeros(imax+layers-1,1);
+x = zeros(imax+layers,1);
 
 %determine material layer thicknesses
 indexx = zeros(length(L)+1,1);
 indexx(1) = 1;
 indexx(2:end) = int32(cumsum(L/dx))+1;
 for j = 1:length(L);
-    kvoid(indexx(j)+j-1:indexx(j+1)+j-2) = layup(j,2);
-    rhovoid(indexx(j)+j-1:indexx(j+1)+j-2) = layup(j,3);
-    cpvoid(indexx(j)+j-1:indexx(j+1)+j-2) = layup(j,4);
-    k(indexx(j):indexx(j+1)-1) = layup(j,2);
-    rho(indexx(j):indexx(j+1)-1) = layup(j,3);
-    cp(indexx(j):indexx(j+1)-1) = layup(j,4);
+    k(indexx(j)+j-1:indexx(j+1)+j-2) = layup(j,2);
+    rho(indexx(j)+j-1:indexx(j+1)+j-2) = layup(j,3);
+    cp(indexx(j)+j-1:indexx(j+1)+j-2) = layup(j,4);
+    x(indexx(j)+j-1:indexx(j+1)+j-1) = dx*[indexx(j)-1:indexx(j+1)-1];
 end
-for j = 1:voidlayers
-    kvoid(indexx(j+1)+j-1) = kfact(j)*(kvoid(indexx(j+1)+j-2)+kvoid(indexx(j+1)+j))/(kvoid(indexx(j+1)+j-2)*kvoid(indexx(j+1)+j));
-    rhovoid(indexx(j+1)+j-1) = (rhovoid(indexx(j+1)+j-2)+rhovoid(indexx(j+1)+j))/2;
-    cpvoid(indexx(j+1)+j-1) = (cpvoid(indexx(j+1)+j-2)+cpvoid(indexx(j+1)+j))/2;
+for j = 1:layers
+    k(indexx(j+1)+j-1) = kfact(j)*(k(indexx(j+1)+j-2)+k(indexx(j+1)+j))/(k(indexx(j+1)+j-2)*k(indexx(j+1)+j));
+    rho(indexx(j+1)+j-1) = (rho(indexx(j+1)+j-2)+rho(indexx(j+1)+j))/2;
+    cp(indexx(j+1)+j-1) = (cp(indexx(j+1)+j-2)+cp(indexx(j+1)+j))/2;
 end
-alphavoid = kvoid./rhovoid./cpvoid;
-vvoid = alphavoid*dt/dx/dx;
-
 alpha = k./rho./cp;
 v = alpha*dt/dx/dx;
 
 
-
-%% Implement Cranck-Nickelson
+%% Implement Cranck-Nickelson with voids
 
 % i is space, n is time
 % space is rows, time is columns
-T = zeros(imax,nmax);
+T = zeros(imax+layers,nmax);
 T(:,1) = T0;
-Tamb = ones(nmax,1)*T0;
-qs = ones(nmax,1)*q0;
-qs(tdur/dt:end) = 0;
+qs = interp1(timeq,qaero,t)*10000;
+Tamb = interp1(timeq,Tatm,t);
 
 
-w = zeros(imax,1);
+w = zeros(imax+layers,1);
 w(1:end-1) = v/2;
 w(2:end) = w(2:end) + v/2;
 C = diag(1+w) - diag(0.5*v,1) - diag(0.5*v,-1);
 N = diag(1-w) + diag(0.5*v,1) + diag(0.5*v,-1);
 
 %define matrices
-A = zeros(imax,1); 
+A = zeros(imax+layers,1); 
 Cinv = inv(sparse(C));
 G = full(Cinv*sparse(N));
 
 for n=1:nmax-1
-    qr = -emiss*sig*(T(1,n)^4-Tamb(n)^4);
-    A(1) = ((qs(n)+qr)/k(1))*v(1)*dx;
+    qrs = -emiss*sig*(T(1,n)^4-Tamb(n)^4);
+    qrb = -emiss*sig*(T(end,n)^4-Tamb(n)^4);
+    A(1) = ((qs(n)+qrs)/k(1))*v(1)*dx;
+    A(end) = (qrb/k(end))*v(end)*dx;
     H = full(Cinv*sparse(A));
     T(:,n+1) = G*T(:,n) + H;
-end
-
-%% Implement Cranck-Nickelson with voids
-
-% i is space, n is time
-% space is rows, time is columns
-S = zeros(imax+voidlayers,nmax);
-S(:,1) = T0;
-Tamb = ones(nmax,1)*T0;
-qs = ones(nmax,1)*q0;
-qs(tdur/dt:end) = 0;
-
-
-wvoid = zeros(imax+voidlayers,1);
-wvoid(1:end-1) = vvoid/2;
-wvoid(2:end) = wvoid(2:end) + vvoid/2;
-C = diag(1+wvoid) - diag(0.5*vvoid,1) - diag(0.5*vvoid,-1);
-N = diag(1-wvoid) + diag(0.5*vvoid,1) + diag(0.5*vvoid,-1);
-
-%define matrices
-A = zeros(imax+voidlayers,1); 
-Cinv = inv(sparse(C));
-G = full(Cinv*sparse(N));
-
-for n=1:nmax-1
-    qrs = -emiss*sig*(S(1,n)^4-Tamb(n)^4);
-    qrb = -emiss*sig*(S(end,n)^4-Tamb(n)^4);
-    A(1) = ((qs(n)+qrs)/kvoid(1))*vvoid(1)*dx;
-    A(end) = (qrb/kvoid(end))*vvoid(end)*dx;
-    H = full(Cinv*sparse(A));
-    S(:,n+1) = G*S(:,n) + H;
 end
 
 % 
@@ -178,47 +149,66 @@ end
 %     layernames{j} = strcat('Layer',int2str(j));
 % end
 % output = table(L*1000,results,layup(:,2),layup(:,3),layup(:,4),'RowNames',layernames,'VariableNames',{'Thickness','maxT','k','rho','cp'});
-%     
-%% Validation
-valid = 1;
-% if valid
-%     valres = dlmread('layup1res.txt');
-%     figure;
-%     hold on
-%     plot(valres(:,1),valres(:,2),'b-')
-%     plot(valres(:,1),valres(:,4),'r-')
-% %     for j = 2:length(valres(1,:))
-% %         plot(valres(:,1),valres(:,j))
-% %     end
-%     plot(t,T(1,:),'b--')
-%     plot(t,T(3,:),'r--')
-% end
-if valid
-    valres = dlmread('layup1res.txt');
+%
+
+%% Contour Plot
+contourplot = 1;
+if contourplot
+    xcont = x;
+    for j = 1:length(L)-1
+        xcont(indexx(j+1)+j-1:indexx(j+1)+j) = [x(indexx(j+1)+j-1)-dx/100,x(indexx(j+1)+j)+dx/100];
+    end
     figure;
     hold on
-    plotS = zeros(nmax,8);
-    plotS(:,1) = S(1,:).';
-    for j = 2:length(indexx)-1
-        plotS(:,2*j-2:2*j-1) = [S(indexx(j)+j-3,:).',S(indexx(j)+j-1,:).'];
+    contourf(t,xcont,T)
+    colormap parula
+	colorbar
+    title('Temperature over time and distance')
+    xlabel('Time [s]')
+    ylabel('Depth [m]')
+    for j = 1:length(L)-1
+        plot([0,nmax*dt],[x(indexx(j+1)+j),x(indexx(j+1)+j)],'--','Color', [255.0/256.0,130.0/256.0,28.0/256.0])
     end
-    plotS(:,8) = S(end,:).';
+    axis ij   
+end
+
+
+%% Layer analysis
+results = zeros(length(L),1);
+layernames = cell(length(L),1);
+for j=1:length(L)
+    results(j) = max(max(T(indexx(j)+j-1:indexx(j+1)+j-1,:)));
+    layernames{j} = strcat('Layer',int2str(j));
+end
+output = table(layup(:,1)*1000,results,layup(:,6),layup(:,2),layup(:,3),layup(:,4),'RowNames',layernames,'VariableNames',{'Thickness','maxT','allowT','k','rho','cp'});
+
+
+%% Validation
+valid = 0;  
+if valid
+    valres = dlmread('layup1res.txt');
+    plotS = zeros(nmax,8);
+    plotS(:,1) = T(1,:).';
+    for j = 2:length(indexx)-1
+        plotS(:,2*j-2:2*j-1) = [T(indexx(j)+j-3,:).',T(indexx(j)+j-1,:).'];
+    end
+    plotS(:,8) = T(end,:).';
     plotVAL = zeros(nmax,8);
     for j = 2:length(valres(1,:))
         plotVAL(:,j-1) = interp1(valres(:,1),valres(:,j),t).';
     end
-    
-    
-    plot(t,plotS,'--')
-    ax = gca;
-    ax.ColorOrderIndex = 1;
-    plot(t,plotVAL)
-    %figure;
-    %plot(t,abs(plotS-plotVAL)./plotVAL)
-    mean(abs(plotS-plotVAL)./plotVAL)
+%     figure;
+%     hold on
+%     plot(t,plotS,'--')
+%     ax = gca;
+%     ax.ColorOrderIndex = 1;
+%     plot(t,plotVAL)
+%     figure;
+%     plot(t,abs(plotS-plotVAL)./plotVAL)
+%     max(abs(plotS-plotVAL)./plotVAL)
 end
     
-    
+disp(output)    
     
     
     
