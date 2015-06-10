2d064f8c02b88a06e62b2ac66d898f7e5db96468
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2015-06-10 17:56:34 +0200

    TD first version

---
+++ SOFTWARE/mars_atmosphere/parachuteestimator.m
@@ -0,0 +1,33 @@
+addpath('../matlab2tikz');
+
+aer = marsatmosphere();
+
+
+%Initial parameters
+m0 = 10000;
+h0 = 10000;
+M0 = 5;
+V0 = aer.getCheapSpeedofsound(0)*M0;
+
+%Energy calculation
+Epot0 = m0*aer.getg(0)*h0;
+Ekin0 = 0.5*m0*V0^2;
+Etot0 = Epot0+Ekin0;
+
+%Velocity when no parachutes are used
+Vnodeceleration = sqrt(2*Etot0/m);
+
+%Just retropropulsion
+Ve = 4400;
+fuelfraction = 1-exp(-Vnodeceleration/Ve);
+fuelmass = m0*fuelfraction;
+
+disp(strcat('Velocity at start (10km): ', num2str(V0)));
+disp(strcat('Velocity at surface without deceleration:', num2str(Vnodeceleration)));
+disp(strcat('Fuel mass fraction when only retropropulsion is used:', num2str(fuelfraction)));
+disp(strcat('Total fuel mass when only retropropulsion is used:', num2str(fuelmass)));
+
+h = 0:10000;
+speedofsound = aer.getCheapSpeedofsound(h);
+plot(h, speedofsound);
+% matlab2tikz('plots/LDplot.tikz','height','\figureheight','width','\figurewidth','showInfo', false,'checkForUpdates',false);
