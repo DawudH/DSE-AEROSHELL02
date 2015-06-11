160b750be6a493790095dda845ae57e46f4bde64
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2015-06-11 11:44:56 +0200

    parachute script updated

--- SOFTWARE/Aero model/globalParams.m
+++ SOFTWARE/Aero model/globalParams.m
@@ -2,10 +2,10 @@ function [ out ] = globalParams()
 %GLOBALPARAMS Summary of this function goes here
 
 out.radius = 6;
-out.LoverD = 0.3;
+out.LoverD = 0.5;
 out.q = 31;
 out.r_capsule = 2.5;
-out.allowskewness = false;
+out.allowskewness = true;
 out.maxheightfactor = 2;
 out.minheightfactor = 0.01;
 out.Cmalpharequired = -0.8;
