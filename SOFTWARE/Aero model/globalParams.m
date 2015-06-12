a3726b10afb83ce00a001788c71119e1c5a7269f
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2015-06-12 13:13:07 +0200

    parallel

--- SOFTWARE/Aero model/globalParams.m
+++ SOFTWARE/Aero model/globalParams.m
@@ -2,12 +2,12 @@ function [ out ] = globalParams()
 %GLOBALPARAMS Summary of this function goes here
 
 out.radius = 6;
-out.LoverD = 0.4;
+out.LoverD = 0.2;
 out.q = 31;
 out.r_capsule = 2.5;
 out.allowskewness = true;
-out.maxheightfactor = 1;
-out.minheightfactor = 0.01;
+out.maxheightfactor = 1.5;
+out.minheightfactor = 0.1;
 out.Cmalpharequired = -0.1;
 out.thetamin = 50;
 out.alphamax = 40;
