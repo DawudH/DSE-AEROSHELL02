42286742a0b06ce4c6bced3d48eb97d474bf3591
Twan Keijzer <twankeijzer@hetnet.nl>
2015-06-15 11:29:15 +0200

    kiek m goaaan

--- SOFTWARE/orbital_model/Frisse Start/delta_v.m
+++ SOFTWARE/orbital_model/Frisse Start/delta_v.m
@@ -4,6 +4,7 @@ close all
 
 %%Input constants & variables
 variables
+h = 150e3;
 
 % booleans
 use_control = false;
@@ -16,10 +17,10 @@ hypkep = false;
 %[out] = full_orbit(R, V, A, G, M_mars, R_m, h_atm, atm, dt_kep_init, dt_atmos, m, omega_m, S, control, tend, crash_margin, g_earth, aero_coef, use_control, multiple_orbits, use_alpha_profile,r,v,theta0,gamma,hypkep,Crho,control.alpha_init,control.dalphadt);
 load('../../Aero model/orbits/orbit_iteration_1_0.mat')
 mu = G*M_mars;
-r = out.okep.a*(1-out.okep.e^2)/(1-out.okep.e)
-v = sqrt(mu*(2/r-1/out.okep.a))
-a_n = (R_m+h_atm+r)/2;
-v_n = sqrt(mu*(2/r-1/a_n))
+r = out.okep.a*(1-out.okep.e^2)/(1-out.okep.e);
+v = sqrt(mu*(2/r-1/out.okep.a));
+a_n = (R_m+h+r)/2;
+v_n = sqrt(mu*(2/r-1/a_n));
 %v_c = sqrt(mu/r)
 %dv_c = v_c-v
 %v_n_p = sqrt(mu*(2/(R_m+h_atm)-1/a_n))
