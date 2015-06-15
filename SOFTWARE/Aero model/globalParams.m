4697d306dcbfa7a1948d0aa715601601034aa1eb
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2015-06-15 15:15:30 +0200

    minor changes

--- SOFTWARE/Aero model/globalParams.m
+++ SOFTWARE/Aero model/globalParams.m
@@ -2,11 +2,12 @@ function [ out ] = globalParams()
 %GLOBALPARAMS Summary of this function goes here
 
 out.radius = 6;
-out.LoverD = 0.2;
+out.LoverD = 0.4;
+% out.CDrequired = 1.3;
 out.q = 31;
 out.r_capsule = 2.5;
 out.allowskewness = true;
-out.maxheightfactor = 1.5;
+out.maxheightfactor = 2;
 out.minheightfactor = 0.1;
 out.Cmalpharequired = -0.1;
 out.thetamin = 70;
