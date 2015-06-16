6dd21cab3b58a5d758a06d16da1774ee8c5d271e
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2015-06-16 14:59:18 +0200

    new heatfluxes

--- SOFTWARE/Aero model/generateheatflux.m
+++ SOFTWARE/Aero model/generateheatflux.m
@@ -1,52 +1,54 @@
 clear;
 close all;
 clc;
-casename = 'orbit_iteration_1_1_entry';
-load(strcat('orbits\',casename,'.mat'));
-load('aeroshapes\iteration1_1_heatflux.mat');
+casename = 'iteration1_1_orbit';
+load(strcat('aeroshapes\',casename,'.mat'));
+skewnessoriginal = x(1);
+heightfactor = x(2);
 
-range = 1:length(out.tp);
+poly = [x(3:end),0,0];   
 
-t = out.tp;
-rho = out.rho;
-T = out.T;
-speed_sound = out.speed_sound;
-M = out.M;
-V = M.*speed_sound;
+for diameter = 18
+    diameter
+    load(strcat('orbits\out_d', num2str(diameter), '_one_time.mat'));    
+    clear mod;
+    radius = diameter/2;
+    height = radius*heightfactor;
 
-gamma = 1.29;
-alpha = out.alpha;
-beta = 0;
-phi = 0;
-q = 31;
-radius = 15;
-qmax_array = zeros(size(t));
-Tboundary = zeros(size(t));
 
-Nr = q;
-a = 0;
-h = 0.5*radius;
-poly = [1 0 0];
-center = [0 0 0];
+    range = 1:length(out.tp);
 
-% [ TriGeom, A, center ] = generategeometry( q, radius );
-% geom = aeroGeometry(TriGeom, A, poly);
-% mod = modnewtonian(geom, gamma, a, center, rho, T);
-geom = mod.geom;
+    t = out.tp;
+    rho = out.rho;
+    T = out.T;
+    speed_sound = out.speed_sound;
+    M = out.M;
+    V = M.*speed_sound;
 
-parfor i = range
-    if rho(i) > 1e-14
-        mod = modnewtonian(geom, gamma, speed_sound(i), center, rho(i), T(i));
-        mod = mod.calcAeroangle(V(i),deg2rad(alpha(i)), beta, phi);
-        [Tmax, qmax] = mod.calcStagnationHeatFlux();
-        Tmax = Tmax(end);
-        qmax = qmax(end);
-        qmax_array(i) = qmax;
-        Tboundary(i) = Tmax;
-        disp(strcat('current number: (', num2str(i), '/', num2str(max(range)), '), qmax: ', num2str(qmax_array(i))));  
+    gamma = 1.29;
+    alpha = out.alpha;
+    beta = 0;
+    phi = 0;
+    q = 31;
+    qmax_array = zeros(size(t));
+    Tboundary = zeros(size(t));
+
+    [ mod, center ] = generateGeometry(poly, q, skewness, radius, height, speed_sound(1), gamma, rho(1), T(1));
+    geom = mod.geom;
+    
+
+    parfor i = range
+        if rho(i) > 1e-14
+            mod = modnewtonian(geom, gamma, speed_sound(i), center, rho(i), T(i));
+            mod = mod.calcAeroangle(V(i),deg2rad(alpha(i)), beta, phi);
+            [Tmax, qmax] = mod.calcStagnationHeatFlux();
+            qmax_array(i) = qmax;
+            Tboundary(i) = Tmax;
+%             disp(strcat('current number: (', num2str(i), '/', num2str(max(range)), '), qmax: ', num2str(qmax_array(i))));  
+        end
     end
+    A_whetted = sum(geom.areas);
+    disp('Finished!');
+    save(strcat('heatflux\heatflux_out_d', num2str(diameter), '_one_time.mat'));
+    plot(t, qmax_array);
 end
-A_whetted = sum(geom.areas);
-disp('Finished!');
-save(strcat('heatflux/heatflux_',casename,'.mat'));
-plot(t, qmax_array);
